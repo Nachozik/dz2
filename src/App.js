269b4afe8a336af23f7170787db70bb2e0cf53ea
Nachozik <eyeless.sad.u@gmail.com>
2022-06-01 20:05:50 +0300

    two

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Component from "./component/component";
+import Component2 from "./component/component2";
 import React from "react";
 
 function App() {
   return (
     <div className="App">
       <Component/>
+      <Component2/>
     </div>
   );
 }
---
+++ src/component/component2.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Component2 = () => {
+    return <div>
+        How are you?
+    </div>
+};
+export default Component2;
